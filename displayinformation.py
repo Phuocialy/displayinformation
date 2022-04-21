88f99172a55bb8d9d05abada1c2650547e7892f3
Phuocialy <phuocnv2501@gmail.com>
2022-04-21 21:58:13 +0700

    Chuong trinh in thong tin ca nha

---
+++ displayinformation.py
@@ -0,0 +1,4 @@
+print("Họ và tên: Nguyễn Văn Phước")
+print("Ngày sinh: 10/01/1998")
+print("Địa chỉ: 33 Bàu Trảng 7, Đà Nẵng")
+print("Mục đích đến với khóa học: Muốn tìm hiểu thêm về lập trình phục vụ cho công việc hiện tại")
